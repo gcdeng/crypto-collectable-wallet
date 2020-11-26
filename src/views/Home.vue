bcbca4504060ed50c3803aec73400ee52319aaff
gcdeng <ericdenggc@gmail.com>
2020-11-27 02:23:00 +0800

    add list page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,95 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <el-row :gutter="20" type="flex">
+      <el-col
+        :xs="12"
+        :sm="8"
+        :md="6"
+        :lg="4"
+        v-for="asset in assets"
+        :key="asset.id"
+      >
+        <router-link
+          :to="`/detail/${asset.assetContractAddress}/${asset.tokenId}`"
+        >
+          <el-card>
+            <img :src="asset.imageUrl" class="image" />
+            <p class="name">{{ asset.name }}</p>
+          </el-card>
+        </router-link>
+      </el-col>
+      <infinite-loading
+        spinner="spiral"
+        @infinite="infiniteHandler"
+      ></infinite-loading>
+    </el-row>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import { Row, Col, Card } from "element-ui";
+import InfiniteLoading from "vue-infinite-loading";
+import getAssets from "@/api/opensea/get-assets";
+
+const mapAsset = asset => ({
+  id: asset.id,
+  imageUrl: asset.image_thumbnail_url,
+  name: asset.name,
+  assetContractAddress: asset.asset_contract.address,
+  tokenId: asset.token_id
+});
 
 export default {
-  name: "Home",
   components: {
-    HelloWorld
+    "el-row": Row,
+    "el-col": Col,
+    "el-card": Card,
+    InfiniteLoading
+  },
+  data() {
+    return {
+      assets: [],
+      offset: 0
+    };
+  },
+  methods: {
+    async infiniteHandler($state) {
+      const { data } = await getAssets({ offset: this.offset });
+      this.offset += data.assets.length;
+      if (data.assets.length > 0) {
+        this.assets.push(...data.assets.map(mapAsset));
+        $state.loaded();
+      }
+      if (data.assets.length < 20) {
+        $state.complete();
+      }
+    }
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.home {
+  margin: 40px;
+  .el-row {
+    flex-wrap: wrap;
+  }
+  .el-col {
+    margin-bottom: 20px;
+  }
+  .el-card {
+    height: 100%;
+  }
+  .image {
+    width: 100%;
+  }
+  .name {
+    text-overflow: ellipsis;
+    width: 100%;
+    overflow: auto;
+  }
+  .infinite-loading-container {
+    width: 100%;
+  }
+}
+</style>
